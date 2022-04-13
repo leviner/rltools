5fec312b54672652055702d0878ffa470718a585
Robert Levine <leviner@uw.edu>
2022-04-12 21:38:28 -0700

    TS processing added

--- ek80Tools/MATLABProcessing/EK80_Bulk_Processing.m
+++ ek80Tools/MATLABProcessing/EK80_Bulk_Processing.m
@@ -1,12 +1,14 @@
+%% Setup
 clear all; close all; clc
-addpath bin
-%%
+addpath lib
 
 dlgTitle    = 'Outputs';
-dlgQuestion = 'Output processed volume scattering?';
-dlg.Sv = questdlg(dlgQuestion,dlgTitle,'Yes','No', 'Yes')
-dlg.TS = 'No';
-
+dlg.Sv = questdlg('Output processed volume scattering?',dlgTitle,'Yes','No', 'Yes')
+dlg.TS = questdlg('Output processed TS?',dlgTitle,'Yes','No', 'Yes')
+if strfind(dlg.Sv,'Yes')
+    dlg.Echo = questdlg('Output Sv Echograms?',dlgTitle,'Yes','No', 'Yes')
+end
+cal = 0 % This is a placeholde RML
 %%% NEED SOMETHING FOR WBT/TUBE
 
 % prompt user for directory for results if not in workspace
@@ -15,6 +17,9 @@ if ~exist('MatOutDir')
 else
     outdir = MatOutDir
 end
+
+
+
 w.z = 40; % Hold depth
 w.S = 32.6; % Hold salinity
 w.T = 4;  % Hold Temp
@@ -45,6 +50,7 @@ for i = 1:length(fbase)
     for channel=1:Nch
         %load([outdir '\'  fchannels(channel).name]);
         range{channel,1} = data.echodata(channel,1).range;
+        
         for ping=1:Npings
             ComplexVoltage(:,ping) = mean(data.echodata(channel,ping).compressed,2);
         end
@@ -52,23 +58,26 @@ for i = 1:length(fbase)
         %time
         time{channel,1}=[data.echodata(channel,:).timestamp];
         
-                para{channel,1}.taueff = data.param(channel, 1).tauEffective;
-        para{channel,1}.ptx = data.param(channel, 1).TransmitPower;
-        para{channel,1}.zet = 75;  % transducer impedance # impedance from the wbat and carry over from the datagram
-        % There are some weird cases in the xml reading where things are
-        % sometimes characters and sometimes doubles so I just make
-        % everything strings RML
-        para{channel,1}.zer = str2num(string(data.config.transceivers(channel).Impedance)); % transceiver imedpance
-        para{channel,1}.psinom = str2num(string(data.config.transceivers(channel).channels.transducer.EquivalentBeamAngle));
-        para{channel,1}.G = getGain(data,channel); % This became complicated because of gain tables
-        
-        para{channel,1}.fsdec = 1/data.param(channel, 1).SampleInterval;
-        para{channel,1}.fstart = data.param(channel,1).FrequencyStart;
-        para{channel,1}.fend = data.param(channel, 1).FrequencyEnd;
-        para{channel,1}.fnom = str2num(string(data.config.transceivers(channel).channels.transducer.Frequency));
-        para{channel,1}.fc = (para{channel}.fstart + para{channel}.fend) ./2;
-        para{channel,1}.alpha = alpha_sea(w.z,w.S,w.T,w.pH,para{channel}.fc/1000); % in dB m
-        
+        if ~cal
+            para{channel,1}.taueff = data.param(channel, 1).tauEffective;
+            para{channel,1}.ptx = data.param(channel, 1).TransmitPower;
+            para{channel,1}.zet = 75;  % transducer impedance # impedance from the wbat and carry over from the datagram
+            % There are some weird cases in the xml reading where things are
+            % sometimes characters and sometimes doubles so I just make
+            % everything strings RML
+            para{channel,1}.zer = str2num(string(data.config.transceivers(channel).Impedance)); % transceiver imedpance
+            para{channel,1}.psinom = str2num(string(data.config.transceivers(channel).channels.transducer.EquivalentBeamAngle));
+            para{channel,1}.sensalong = str2num(string(data.config.transceivers(channel).channels.transducer.AngleSensitivityAlongship));
+            para{channel,1}.sensathw = str2num(string(data.config.transceivers(channel).channels.transducer.AngleSensitivityAthwartship));
+            para{channel,1}.G = getGain(data,channel); % This became complicated because of gain tables
+            
+            para{channel,1}.fsdec = 1/data.param(channel, 1).SampleInterval;
+            para{channel,1}.fstart = data.param(channel,1).FrequencyStart;
+            para{channel,1}.fend = data.param(channel, 1).FrequencyEnd;
+            para{channel,1}.fnom = str2num(string(data.config.transceivers(channel).channels.transducer.Frequency));
+            para{channel,1}.fc = (para{channel}.fstart + para{channel}.fend) ./2;
+            para{channel,1}.alpha = alpha_sea(w.z,w.S,w.T,w.pH,para{channel}.fc/1000); % in dB m
+        end
         
         for ping =1:Npings
             if ~isempty(data.echodata(channel,ping).channelID)
@@ -80,19 +89,17 @@ for i = 1:length(fbase)
                     CV{channel,1}(:,ping) = ComplexVoltage(:,ping);
                 end
                 
-%                 if strmatch(dlg.TS,'Yes')
-%                     [sp, phi] = TSCalc(channel, ComplexVoltage, para, w, range, ping);
-%                     Sp{channel}(:,ping) = sp;
-%                     Phi{channel}(:,ping) = phi;
-%                 end
+                if strmatch(dlg.TS,'Yes')
+                    [sp, phialong, phiathw] = TSCalc(channel, data, para, w, range, ping);
+                    Sp{channel}(:,ping) = sp;
+                    PhiAlong{channel}(:,ping) = phialong;
+                    PhiAthw{channel}(:,ping) = phiathw;
+                end
                 
             end
         end
-        %para{channel}.fnom = fnom;
         
-       
-        
-        if strfind(dlg.Sv,'Yes')
+        if strfind(dlg.Echo,'Yes')
             % [~,fnm,~] =fileparts(rawfiles);
             fout = ['Sv_' fbase{i}];
             h = figure('visible','off','units','inch','position',[2,2,6,3]);
@@ -113,11 +120,12 @@ for i = 1:length(fbase)
             clear fout
             
         end
-        
-        
-    clear ComplexVoltage   
+        clear ComplexVoltage
     end
     
+    
+    % Save the files
+    % If calculating Sv, save separately
     if strfind(dlg.Sv,'Yes')
         % [~,fnm,~] =fileparts(rawfiles);
         fout2 = ['Sv_' fbase{i} '.mat'];
@@ -130,7 +138,20 @@ for i = 1:length(fbase)
         clear fout2
     end
     
-    fclose('all')
+    % If calculating TS, save separately
+    if strfind(dlg.TS,'Yes')
+        % [~,fnm,~] =fileparts(rawfiles);
+        fout2 = ['Sp_' fbase{i} '.mat'];
+        % save([outdir '\Data\' fout2],'Sv','ComplexVoltage','pings','time','range','para','w','gps');
+        if ~exist([outdir '\Data\'])
+            mkdir([outdir '\Data\'])
+        end
+        save([outdir '\Data\' fout2],'Sp','PhiAlong','PhiAthw','time','range','para','w');
+        % need to edit to bring in ship GPS data
+        clear fout2
+    end
+    
+    fclose('all');
     % insert file save here
-    clear Sv CV
+    clear Sv CV Sp PhiAlong PhiAthw
 end
