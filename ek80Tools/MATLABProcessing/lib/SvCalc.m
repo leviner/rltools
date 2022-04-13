d768c8a67f6c5009c5c5686e7d7bd26023679aa0
Robert Levine <leviner@uw.edu>
2022-04-12 21:22:55 -0700

    fix complex TVG

--- ek80Tools/MATLABProcessing/lib/SvCalc.m
+++ ek80Tools/MATLABProcessing/lib/SvCalc.m
@@ -1,5 +1,5 @@
 function [sv] = SvCalc(channel, ComplexVoltage, para, w, range, ping)
-TVG = abs(20*log10(range{channel}) + 2*para{channel}.alpha*range{channel}); % b
+TVG = real(abs(20*log10(range{channel}) + 2*para{channel}.alpha*range{channel})); % b
 yc = ComplexVoltage(:,ping);
 prx = (abs(yc)/(sqrt(2))).^2 * ((para{channel}.zer+para{channel}.zet)/para{channel}.zer).^2*(1/para{channel}.zet);
 sv = 10*log10(prx)  + TVG - 10*log10(para{channel}.ptx*(w.c/para{channel}.fc)^2 * ...
