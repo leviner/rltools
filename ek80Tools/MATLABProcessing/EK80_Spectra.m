cec47158f65749abd3838e6180125cb39e043d08
Robert Levine <leviner@uw.edu>
2022-08-24 15:08:24 -0700

    Add overlap and separate horizontal bin averaging

--- ek80Tools/MATLABProcessing/EK80_Spectra.m
+++ ek80Tools/MATLABProcessing/EK80_Spectra.m
@@ -3,11 +3,12 @@ addpath lib
 addpath('lib/bin')
 addpath('lib/bin/gsw')
 
-prompt = {'Enter window width in pings','Enter window depth in meters'};
+prompt = {'Enter window width in pings','Enter window width overlap in pings','Enter window depth in meters',};
 test = inputdlg(prompt);
 
 win.l = str2num(test{1});       % window length in pings
-win.step = str2num(test{2});   % window step size in meters
+win.overlap = str2num(test{2});  % window overlap in pings
+win.step = str2num(test{3});   % window step size in meters
 win.nfft = 2^10; % current FFT size = 1024 points (about twice as long as number in longest window)
 
 
@@ -18,7 +19,7 @@ load([filepath '\' fn{1}])
 %%
 
 [nChannels,nPings] = size(data.echodata);
-startPings = [1:win.l:nPings];
+startPings = 1:win.l-win.overlap:nPings;
 
 maxr = [];
 for i=1:nChannels
@@ -35,18 +36,18 @@ for iii = 1:length(fn)
     for jjj= 1:nChannels
         clear CV
         t = data.echodata(jjj,:);
-        if isempty(t(1).compressed)
+        if isempty(t(1).complexsamples)
             continue
         else
-        for s =1:nPings
-            CV(:,s) = mean(t(s).compressed,2);
-        end
-        CVAll{jjj,1} = [CV];
+            for s =1:nPings
+                CV(:,s) = mean(t(s).complexsamples,2);
+            end
+            CVAll{jjj,1} = [CV];
         end
     end
 
-    clear Spec f F
-    for j = 1:length(startPings) % for each ping
+    clear Spec f F SpecBins
+    for j = 1:nPings % for each ping
         for jj = 1:length(win.r) % for each range bin
             waitbar(j/length([1:win.l:nPings]),bar,['Calculating spectra for each ' num2str(win.l) ' ping by ' num2str(win.step) ' m bin ' newline 'for file ' num2str(iii) ' of ' num2str(length(fn))]) ;
             for jjj = 1:nChannels % for each channel
@@ -56,43 +57,39 @@ for iii = 1:length(fn)
                     f{jjj} = NaN;
                     continue
                 end
-                
-                timestamp{jjj,j}=data.echodata(jjj,startPings(j)).timestamp;
-                
-                if startPings(j)+win.l-1 < nPings
-                    CVwin = mean(CVAll{jjj,1}(:,startPings(j):startPings(j)+win.l-1),2);
-                else
-                    CVwin = mean(CVAll{jjj,1}(:,startPings(j):nPings),2);
-                end
-                
+
+                timestamp{jjj,j}=data.echodata(jjj,j).timestamp;
+
+                CVwin = CVAll{jjj,1}(:,j); % grab the ping
+
                 CVwinR = CVwin((ranges < win.r(jj)+win.step) &(ranges >= win.r(jj)));
                 specvec = CVwinR.*ranges(((ranges < win.r(jj)+win.step)&(ranges >= win.r(jj))));
                 b = tukeywin(length(specvec),0.1)./(norm(tukeywin(length(specvec),0.1))./sqrt(length(specvec)));
                 specvec = specvec.*b;
-                specvec = fft(specvec,win.nfft);                
-                
+                specvec = fft(specvec,win.nfft);
+
                 fsdec = 1/data.param(jjj, 1).SampleInterval;
-               
-                
+
+
                 if isstr(data.config.transceivers(jjj).channels.transducer.Frequency)
                     fnom = str2num(data.config.transceivers(jjj).channels.transducer.Frequency);
                 else
                     fnom = data.config.transceivers(jjj).channels.transducer.Frequency;
                 end
-                
+
                 [ftmp, FFTvec_tmp] = freqtransf(specvec,fsdec,fnom);
                 alphaf =  alpha_sea(data.environ.Depth,data.environ.Salinity,data.environ.Temperature,data.environ.Acidity,ftmp/1000);
                 calf = data.calibration(jjj).Frequency;
                 calg = data.calibration(jjj).Gain;
                 dens = gsw_rho(data.environ.Salinity,data.environ.Temperature,data.environ.Depth);
                 c = gsw_sound_speed(data.environ.Salinity,data.environ.Temperature,dens*9.81*data.environ.Depth*1e-4);
-                
+
                 if isstr(data.config.transceivers(jjj).channels.transducer.EquivalentBeamAngle)
                     calpsi = str2num(data.config.transceivers(jjj).channels.transducer.EquivalentBeamAngle);
                 else
                     calpsi = data.config.transceivers(jjj).channels.transducer.EquivalentBeamAngle;
                 end
-                
+
                 G = interp1(calf,calg,ftmp);
                 psi =calpsi + 20*log10(fnom./ftmp);
                 dt =2*((win.r(jj)+win.step)-win.r(jj))/c;
@@ -108,21 +105,45 @@ for iii = 1:length(fn)
                     10.*log10(c^3./(32.*pi^2.*ftmp.^2));
                 svtmp{jjj} = sv';
                 f{jjj} = ftmp';
-               if j == 1
-               Gs{jjj}=G;
-               end
-                
-                
+                if j == 1
+                    Gs{jjj}=G;
+                end
+
+
             end
             Spec{jj,j} = [svtmp{:}];
             F{jj,j} =  [f{:}];
 
-            
+
         end
     end
- 
-fout = [outdir '\Spectra_' char(fn(iii))];
-save(fout, 'Spec','F','win','timestamp')
-    
+
+    % Now we make the average for the ping (horizontal) bins
+    waitbar(1,bar,'Calcuating horizontal bin averages...');
+    for p = 1:length(startPings)
+        if startPings(p)+win.l-1 > nPings
+            test = Spec(:,startPings(p):nPings);
+        else
+            test = Spec(:,startPings(p):startPings(p)+win.l-1);
+        end
+
+        for tt= 1:size(test,1)
+            curBin = test(tt,:);
+            h = 0;
+            for v = 1:length(curBin)
+                h = h+10.^(curBin{v}./10);
+            end
+            h2 = h./length(curBin);
+            h2 = 10.*log10(h2);
+
+            SpecBins{tt,p} = h2;
+        end
+    end
+
+    waitbar(1,bar,['Saving file '  num2str(iii) ' of ' num2str(length(fn))]);
+    fout = [outdir '\Spectra_Complex_' num2str(win.l)...
+        'p' num2str(win.step) 'm' num2str(win.overlap) 'overlap_' char(fn(iii))];
+    save(fout, 'Spec','SpecBins','F','win','timestamp')
+
 end
 waitbar(1,bar,'Done') ;
